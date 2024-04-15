effc9d531d835550807afc1310e865fdf3208d7f
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2024-04-15 10:13:02 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+    basic.showString("A")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+    basic.showString("B")
+})
+radio.setGroup(240)
